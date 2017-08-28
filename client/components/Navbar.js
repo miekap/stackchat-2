fb20f97c8e722be90b0f62d643c5f8d09d8b31b2
Mieka Page <git@coldwine.nyc>
2017-08-28 17:26:49 -0400

    navbar updates as channel is typed

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -6,10 +6,14 @@ import { connect } from 'react-redux'
 function Navbar(props, ownProps) {
 
   const channelId = Number(props.match.params.channelId);
+  let title = '';
+    if (props.channels.length && channelId) title = props.channels.filter(channel => channel.id == channelId)[0].name;
+    else if (!props.newChannelEntry && !channelId) title = 'Create Channel';
+    else if (props.newChannelEntry && !channelId) title = props.newChannelEntry;
 
   return (
     <nav>
-      <h3>{(props.channels.length && channelId) ? props.channels.filter(channel => channel.id == channelId)[0].name : 'Create Channel'}</h3>
+      <h3>{title}</h3>
       <NameEntry />
     </nav>
   );
@@ -18,6 +22,7 @@ function Navbar(props, ownProps) {
 const mapStateToProps = function (state, ownProps) {
   return {
     channels: state.channels,
+    newChannelEntry: state.newChannelEntry,
     ownProps
   };
 }
